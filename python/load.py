1d65c8152b4eb2e3379cfcf8e1473c151877f0f4
sankra <sankranthivijayasai@gmail.com>
2025-05-11 09:43:39 -0700

    main.py file updated

--- python/load.py
+++ python/load.py
@@ -11,16 +11,16 @@ def load_data_to_s3():
     # Load the CSV file into a DataFrame
     df = pd.read_csv(file_path)
 
-# def load_data_to_redshift():
-#     # Define the Redshift connection parameters
-#     redshift_conn = {
-#         "db_user": "your-db-user",
-#         "db_password": "your-db-password",
-#         "db_name": "your-db-name",
-#         "db_port": 5439,
-#         "db_host": "your-db-host",
-#         "db_table": "your-db-table"
-#     }
+def load_data_to_redshift():
+    # Define the Redshift connection parameters
+    redshift_conn = {
+        "db_user": "your-db-user",
+        "db_password": "your-db-password",
+        "db_name": "your-db-name",
+        "db_port": 5439,
+        "db_host": "your-db-host",
+        "db_table": "your-db-table"
+    }
 
 if __name__ == "__main__":
     # Load data to S3
